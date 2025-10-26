a29fb8c98e60d72ff21a531ab381a542f5f905a0
ianache <ianache@comsatel.com.pe>
2025-10-26 13:51:00 -0500

    add: inicial

--- src/test/java/com/crossnetcorp/agentic/AgenticFlowTest.java
+++ src/test/java/com/crossnetcorp/agentic/AgenticFlowTest.java
@@ -8,6 +8,7 @@ import java.util.stream.Stream;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,6 +16,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.crossnetcorp.GeneralIntegrationFlow;
 import com.crossnetcorp.transformation.TestInput;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Test class for the Velocity flow.
@@ -34,9 +39,19 @@ public class AgenticFlowTest {
         GeneralIntegrationFlow flow = new GeneralIntegrationFlow(data.flow, "ingestQdrant");
         Object result = flow.handle(
             data.request,
-            (message) -> {
+            /*(message) -> {
                 message.setPayload(message.getPayload().toString());
                 logger.info(message.getPayload().toString());
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode _obj = mapper.readTree(message.getPayload().toString());
+                return message;
+            },*/
+            (message) -> {
+                String body = buildPoint(
+                    ((JSONObject)message.getPayload()).get("embedding").toString(), 
+                    ((JSONObject)message.getPayload()).get("content").toString()
+                );
+                logger.info(body);
                 return message;
             }
         );
@@ -58,5 +73,24 @@ public class AgenticFlowTest {
             )
         );
         return data.stream();
+    }
+
+    private static String buildPoint(String vector, String payload) {
+        return String.format("""
+            {
+          "wait": true,
+          "points": [
+            {
+              "id": 1,
+              "vector": %s,
+              "payload": {
+                 "text": "%s",
+                 "author": "Ilver Anache Pupo",
+                 "source": "Caso de Prueba"
+              }
+            }
+          ]
+        }
+          """,vector,payload);
     }
 }
