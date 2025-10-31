f7c8ca002f5574e81c6221ecc2bdf5c21f35e7a6
ianache <ianache@comsatel.com.pe>
2025-10-31 16:27:32 -0500

    documentacion y ajusta k6

--- test/load/k6.js
+++ test/load/k6.js
@@ -2,11 +2,26 @@ import http from 'k6/http';
 import { check, group, sleep } from 'k6';
 
 export const options = {
-  vus: 2,
-  duration: '10s',
+  scenarios: {
+    constant_request_rate: {
+      // Peticiones a tiempo fijo
+      executor: 'constant-arrival-rate',
+      // Tasa de llegada deseada: 1 iteración/segundo (0.1 por segundo)
+      // 1 iteración / 10 segundos = 0.1 iteraciones/segundo
+      rate: 1,
+      // La unidad de tiempo para la 'rate' (0.1 iteraciones cada 1 segundo)
+      timeUnit: '1s', 
+      // Duración total de la prueba
+      duration: '1m', 
+      // Número de VUs iniciales para que la prueba comience rápidamente
+      preAllocatedVUs: 1,
+    }
+  },
+  //vus: 1,
+  //duration: '60s',
   thresholds: {
     http_req_duration: ['p(95)<600000'], // 95 percent of response times must be below 10000ms (10s)
-  },
+  }
 };
 
 const SLEEP_DURATION = 0.1;
@@ -18,10 +33,11 @@ export function setup() {
 }
 
 export default function(data) {
-  const url = 'http://localhost:8080/gateway/api/v1/logistica/programacion';
+  const url = 'http://192.168.56.101:8086/gateway/api/v1/logistica/programacion';
   const params = {
     headers: {
       'Content-Type': 'application/json',
+      "X-Forwarded-For": "127.0.0.1"
       // 'Authorization': `Bearer ${data.access_token}`
     },
   };
@@ -31,5 +47,5 @@ export default function(data) {
     'is status 200': (r) => r.status === 200,
   });
   
-  sleep(SLEEP_DURATION);
+  // sleep(SLEEP_DURATION);
 }
